3983f6b7b483ac2b64d0a3df2237ff871613197d
Matt <lemontongs@gmail.com>
2018-03-04 12:12:55 -0500

    small changes

--- OS_Program2.c
+++ OS_Program2.c
@@ -7,6 +7,9 @@
 
 #define MAX_QUEUE_SIZE 1000
 
+// debug
+int counter = 1;
+
 
 typedef struct
 {
@@ -33,37 +36,83 @@ void Queue_Create(Queue* buff, uint32_t buff_size)
     buff->write = 0;
 }
 
+void Queue_Print(Queue* buff)
+{
+    uint32_t ii;
+    char* str = "";
+    
+    // Build the string
+    asprintf(&str, "QUEUE: r:%d w:%d s:%d data:", buff->read, buff->write, buff->size);
+    for(ii = 0; ii < buff->size; ii++)
+    {
+        asprintf(&str, "%s%d ", str, buff->data[ii]);
+    }
+    asprintf(&str, "%s\n", str);
+    
+    // Print
+    printf(str);
+}
+
+uint32_t Queue_Full(Queue* buff)
+{
+    return (buff->read + 1) % buff->size == buff->write;
+}
+
+uint32_t Queue_Empty(Queue* buff)
+{
+    return buff->write == buff->read;
+}
+
 void Queue_Enqueue(Queue* buff, uint32_t val)
 {
-    /* Wait for room */
-    while(buff->write != buff->read);
+    // Wait until not full
+    while(Queue_Full(buff));
     
+    // Lock
     pthread_mutex_lock(&buff->mutex);
+    
+    // Modify
     buff->data[buff->write] = val;
     buff->write = (buff->write + 1) % buff->size;
+    
+    // Print status
+    Queue_Print(buff);
+    
+    // Unlock
     pthread_mutex_unlock(&buff->mutex);
 }
 
 void Queue_Dequeue(Queue* buff, uint32_t* val)
 {
-    /* Wait for data */
-    while(buff->read == buff->write);
+    // Wait for something to consume
+    while(Queue_Empty(buff));
     
+    // Grab the lock
     pthread_mutex_lock(&buff->mutex);
+    
+    // Get value at read address
     *val = buff->data[buff->read];
+    
+    // Increment read address
     buff->read = (buff->read + 1) % buff->size;
+    
+    // Print status
+    Queue_Print(buff);
+    
+    // Unlock
     pthread_mutex_unlock(&buff->mutex);
 }
-
 
 void* producer(void* arg)
 {
     Queue* buff = (Queue*)arg;
     uint32_t new_value = rand() % 10;
     
-    printf("producer %d\n", new_value);
-
+    new_value = counter++;
+    
+    printf("producer %p: produced %d\n", pthread_self(), new_value);
     Queue_Enqueue(buff, new_value);
+    printf("producer %p DONE\n", pthread_self());
 }
 
 void* consumer(void* arg)
@@ -71,9 +120,9 @@ void* consumer(void* arg)
     uint32_t val;
     Queue* buff = (Queue*)arg;
 
+    printf("consumer %p dequeue\n", pthread_self());
     Queue_Dequeue(buff, &val);
-    
-    printf("consumer %d\n", val);
+    printf("consumer %p got %d\n", pthread_self(), val);
 }
 
 
@@ -107,7 +156,8 @@ int main(int argc, void** argv)
     
     /* Create the shared queue */
     Queue_Create(&q, buff_size);
-        
+    Queue_Print(&q);
+    
     /* Create threads */
     pthread_t threads[num_threads];
     for(id = 0; id < num_threads; id++)
@@ -127,14 +177,16 @@ int main(int argc, void** argv)
             printf("Error creating thread\n");
             return 1;
         }
+        pthread_join(threads[id], NULL);
+        
     }
     
-    /* Waif for all the threads to finish */
+    /* Wait for all the threads to finish
     for(id = 0; id < num_threads; id++)
     {
         pthread_join(threads[id], NULL);
         printf("Thread %d DONE\n", id);
-    }
+    } */
     
     printf("DONE\n");
     return 0;
--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -o prog2 OS_Program2.c -pthread && ./prog2 200 10 1
+gcc -o prog2 OS_Program2.c -pthread && ./prog2 $@
 
